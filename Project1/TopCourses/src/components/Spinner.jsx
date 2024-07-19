9b82bd8f1cf7ed994943b86f43e922cb3aa3fea3
shivansh <shivanshgupta977@gmail.com>
2024-07-19 15:04:21 +0530

    Initial commit with TopCourses project

---
+++ Project1/TopCourses/src/components/Spinner.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Spinner.css";
+function Spinner() {
+  return (
+<>
+<div class="custom-loader">
+    
+    </div>
+    <p>Loading.....</p>
+    </>
+  )
+}
+
+export default Spinner
